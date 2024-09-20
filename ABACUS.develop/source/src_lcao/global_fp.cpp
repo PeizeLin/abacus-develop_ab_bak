71c974efff00ab25117fd3d296f33009cd9ce67a
PeizeLin <78645006+PeizeLin@users.noreply.github.com>
2024-09-21 03:07:49 +0800

    Initialize exx

---
+++ ABACUS.develop/source/src_lcao/global_fp.cpp
@@ -0,0 +1,16 @@
+#include "global_fp.h"
+#include "src_pw/global.h"
+
+#ifdef __FP
+Grid_Driver GridD;
+Parallel_Atoms ParaA;
+Parallel_Orbitals ParaO;
+Local_Orbital_Charge LOC;
+Local_Orbital_wfc LOWF;
+LCAO_Matrix LM;
+Use_Hamilt_Matrix UHM;
+SubGrid_oper SGO; //mohan add 2012-01-12
+Exx_Lcao exx_lcao(exx_global.info); // Peize Lin add 2016-12-03
+
+#endif
+
